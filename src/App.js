3a2f324545ae72b9be3341d83d56dcd068a0882c
sofia durkan <sofiadurkan@gmail.com>
2021-08-30 11:56:45 -0500

    Card.css added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import "./App.css";
 import Card from "./components/Card/Card";
 
 const App = () => {
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,4 +1,6 @@
 import { useState, useEffect } from "react";
+import "./Card.css"
+
 import axios from "axios";
 import Email from "../svg/Email";
 import Location from "../svg/Location";
@@ -6,7 +8,7 @@ import Phone from "../svg/Phone";
 
 const Card = () => {
   const [user, setUser] = useState([]);
-  useState(false)
+  useState(false);
 
  
   const getRandomUser = () => {
@@ -18,7 +20,6 @@ const Card = () => {
 
   useEffect(() => {
     getRandomUser();
-    console.log(getRandomUser())
    
   },[]);
 
@@ -66,3 +67,69 @@ const Card = () => {
 };
 
 export default Card;
+
+
+
+
+//----------------------First Version------------------------//
+
+
+// import { useState, useEffect } from "react";
+// import axios from "axios";
+
+
+
+// const Card = () => {
+//   const [user, setUser] = useState([]);
+//   const [isLoaded, setIsLoaded] = useState(false);
+
+//   useEffect(() => {
+//     axios.get("https://randomuser.me/api/").then((response) => {
+//       // console.log(response.data.results[0])
+//       setUser(response.data.results[0]);
+//     });
+//   }, [isLoaded]);
+
+//   const randomUser = () => {
+//     // console.log(setIsLoaded(!isLoaded));
+//     setIsLoaded(!isLoaded);
+//   };
+
+//   return (
+//     <div className="container">
+//       <div className="card">
+//         <img className="img" src={user?.picture?.large} alt="" />
+//         <p className="full-name">
+//           {user?.title}
+//           {user?.name?.first}
+//           {user?.name?.last}
+//         </p>
+      
+//       <div className="userEmail">
+//         <p>{user.email}</p>
+//       </div>
+//       <div className="userPhone">
+//         <p>{user.phone}</p>
+//       </div>
+//       <div className="userLocation">
+//         <p>
+//           {user?.location?.city} {user?.location?.country}
+//         </p>
+//       </div>
+//       <div>
+//         <p className="userAge">Age:{user.age}</p>
+//         <p className="userRegisterDate">
+//           Register Date:{user?.registered?.date}{" "}
+//         </p>
+//       </div>
+//       <div>
+//         <button className="button" onClick={randomUser}>
+//           Random User
+//         </button>
+//       </div>
+//     </div>
+//     </div>
+//   );
+// };
+
+// export default Card;
