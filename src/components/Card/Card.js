c1de52dac026b439b17c4a02ed526e8c39155d5d
sofia durkan <sofiadurkan@gmail.com>
2021-08-31 09:18:22 -0500

    last edit

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,37 +1,30 @@
 import { useState, useEffect } from "react";
 import "./Card.css";
 import axios from "axios";
-import Email from "../svg/Email";
-import Location from "../svg/Location";
-import Phone from "../svg/Phone";
+import Email from "../Svg/Email";
+import Location from "../Svg/Location";
+import Phone from "../Svg/Phone";
 
 const Card = () => {
-  const [user, setUser] = useState(false);//False=>bos bir array atiyor.
-
-
-
+  const [user, setUser] = useState(false); //False=>bos bir array atiyor.
 
   const getRandomUser = () => {
     axios.get("https://randomuser.me/api/").then((response) => {
-      // console.log(response.data.results[0])
-      console.log(response.data)
+      console.log(response.data.results[0]);
+      // console.log(response.data)
 
       setUser(response.data.results[0]);
     });
   };
 
-
-
   useEffect(() => {
     getRandomUser();
   }, []);
 
-
-
   return (
     <div className="container">
       <div className="card">
-        <div className="first-box">
+        <div className="image-title-box">
           <img className="img" src={user.picture?.large} alt="" />
           <p className="info">
             {user.name?.title} {user.name?.first} {user.name?.last}
@@ -72,62 +65,3 @@ const Card = () => {
 
 export default Card;
 
-//----------------------First Version------------------------//
-
-// import { useState, useEffect } from "react";
-// import axios from "axios";
-
-// const Card = () => {
-//   const [user, setUser] = useState([]);
-//   const [isLoaded, setIsLoaded] = useState(false);
-
-//   useEffect(() => {
-//     axios.get("https://randomuser.me/api/").then((response) => {
-//       // console.log(response.data.results[0])
-//       setUser(response.data.results[0]);
-//     });
-//   }, [isLoaded]);
-
-//   const randomUser = () => {
-//     // console.log(setIsLoaded(!isLoaded));
-//     setIsLoaded(!isLoaded);
-//   };
-
-//   return (
-//     <div className="container">
-//       <div className="card">
-//         <img className="img" src={user?.picture?.large} alt="" />
-//         <p className="full-name">
-//           {user?.title}
-//           {user?.name?.first}
-//           {user?.name?.last}
-//         </p>
-
-//       <div className="userEmail">
-//         <p>{user.email}</p>
-//       </div>
-//       <div className="userPhone">
-//         <p>{user.phone}</p>
-//       </div>
-//       <div className="userLocation">
-//         <p>
-//           {user?.location?.city} {user?.location?.country}
-//         </p>
-//       </div>
-//       <div>
-//         <p className="userAge">Age:{user.age}</p>
-//         <p className="userRegisterDate">
-//           Register Date:{user?.registered?.date}{" "}
-//         </p>
-//       </div>
-//       <div>
-//         <button className="button" onClick={randomUser}>
-//           Random User
-//         </button>
-//       </div>
-//     </div>
-//     </div>
-//   );
-// };
-
-// export default Card;
